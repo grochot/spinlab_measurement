61dffd9d8a0cd2561a510f86d7bdb66c8a12b60e
Mariusz Cierpiał <cierpial@studet.agh.edu.pl>
2024-02-27 12:12:26 +0100

    dodalem pliki

---
+++ hardware/dummy_pulsegenerator.py
@@ -0,0 +1,38 @@
+class DummyPulsegenerator:
+    def __init__(self, resourceName):
+        pass
+
+    
+    
+    
+#--------------------------------tektronix10070a------------------------------------------    
+    def amplitude(amplitude):
+        pass
+
+    
+    def disable():
+        pass
+
+    def enable():
+        pass
+
+    def duration():
+        pass
+
+    def offset(offset):
+        pass
+
+    def period(period):
+        pass
+
+    def reset():
+        pass
+    
+    def give_one_pulse():
+        pass
+
+    def trigger_source(trigger_source):
+        #possible values = "INT", "EXT", "MAN", "GPIB"
+        pass
+#*-------------------------------------------------------------------------------------
+    
---
+++ hardware/tektronix_10070a.py
@@ -0,0 +1,49 @@
+from pymeasure.instruments import Instrument 
+import pyvisa 
+from time import sleep
+import numpy as np
+
+class Tektronix10070a(Instrument):
+    def __init__(self, adapter, read_termination="\n", **kwargs):
+        super().__init__(
+            adapter,
+            "Tektronix 10070a" ,
+            read_termination=read_termination,
+            **kwargs
+        )
+    
+    def amplitude(amplitude):
+        self.write('AMPL %d'%amplitude)
+
+    
+    def disable():
+        self.write('DIS')
+
+    def enable():
+        self.write('ENAB')
+
+    def duration():
+        self.write('DUR')
+
+    def offset(offset):
+        self.write('OFFS')
+
+    def period(period):
+        self.write('PER %d'%period)
+
+    def reset():
+        self.write('*RST')
+    
+    def give_one_pulse():
+        self.write('*TRG')
+
+    def trigger_source(trigger_source):
+        #possible values = "INT", "EXT", "MAN", "GPIB"
+        self.write('TRIG %s'%trigger_source)
+
+    
+
+    
+    
+
+    
