2a062369a805d97dbcffab00531ca743624f7276
dmaslanka <dmaslanka@student.agh.edu.pl>
2024-07-10 14:16:00 +0200

    Add toggle functionality to Water Cooler Control widget, Add state saving

--- modules/control_widget_water_cooler.py
+++ modules/control_widget_water_cooler.py
@@ -5,12 +5,20 @@ import nidaqmx
 class WaterCoolerControl(QtWidgets.QWidget):
     def __init__(self):
         super(WaterCoolerControl, self).__init__()
+        self.state = False
+
+        self.setWindowTitle("Water Cooler Control")
 
         self.address = "Dev1/ao1"
 
         self.setStyleSheet("QLabel { font-size: 14pt; } QPushButton { font-size: 14pt; }")
         
         self.layout = QtWidgets.QGridLayout(self)
+
+        self.toggle_btn = QtWidgets.QPushButton("Toggle")
+        self.toggle_btn.clicked.connect(self.on_toggle)
+        self.layout.addWidget(self.toggle_btn, 1, 1)
+
         self.label = QtWidgets.QLabel("Cooling?")
         self.layout.addWidget(self.label, 1, 2)
 
@@ -19,7 +27,6 @@ class WaterCoolerControl(QtWidgets.QWidget):
         self.layout.addWidget(self.start_btn, 2, 1)
 
         self.stop_btn = QtWidgets.QPushButton("STOP")
-        self.stop_btn.setEnabled(False)
         self.stop_btn.clicked.connect(lambda: self.pulse(False))
         self.layout.addWidget(self.stop_btn, 3, 1)
 
@@ -54,7 +61,38 @@ class WaterCoolerControl(QtWidgets.QWidget):
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.finish_pulse)
 
+        self.loadState()
+        self.updateGUI()
+
+    def closeEvent(self, event):
+        self.saveState()
+        event.accept()
+
+    def updateGUI(self):
+        if self.state:
+            self.on_indicator.setStyleSheet("background: green; border: 3px solid gray;")
+            self.off_indicator.setStyleSheet("border: 3px solid gray;")
+            self.start_btn.setEnabled(False)
+            self.stop_btn.setEnabled(True)
+        else:
+            self.on_indicator.setStyleSheet("border: 3px solid gray;")
+            self.off_indicator.setStyleSheet("background: red; border: 3px solid gray;")
+            self.start_btn.setEnabled(True)
+            self.stop_btn.setEnabled(False)
+
+    def saveState(self):
+        with open('water_cooler_state.txt', 'w') as f:
+            f.write(str(self.state))
+
+    def loadState(self):
+        try:
+            with open('water_cooler_state.txt', 'r') as f:
+                self.state = bool(f.read())
+        except FileNotFoundError:
+            pass
+
     def pulse(self, state):
+        self.state = state
         if state:
             self.on_indicator.setStyleSheet("background: green; border: 3px solid gray;")
             self.off_indicator.setStyleSheet("border: 3px solid gray;")
@@ -77,6 +115,20 @@ class WaterCoolerControl(QtWidgets.QWidget):
         with nidaqmx.Task() as task:
             task.ao_channels.add_ao_voltage_chan(self.address, min_val=0, max_val=5)
             task.write(0)
+
+    def on_toggle(self):
+        self.state = not self.state
+        if self.state:
+            self.on_indicator.setStyleSheet("background: green; border: 3px solid gray;")
+            self.off_indicator.setStyleSheet("border: 3px solid gray;")
+            self.start_btn.setEnabled(False)
+            self.stop_btn.setEnabled(True)
+        else:
+            self.on_indicator.setStyleSheet("border: 3px solid gray;")
+            self.off_indicator.setStyleSheet("background: red; border: 3px solid gray;")
+            self.start_btn.setEnabled(True)
+            self.stop_btn.setEnabled(False)
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
