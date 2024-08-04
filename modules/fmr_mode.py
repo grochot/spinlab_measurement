3f06ddf7ddc9f3bf08f4eb2706a02a51b80cb692
Krzysztof Grochot <grochot@agh.edu.pl>
2024-08-04 23:54:35 +0200

    fix fmr mode

--- modules/fmr_mode.py
+++ modules/fmr_mode.py
@@ -68,7 +68,9 @@ class FMRMode():
         constant_field_value:float, 
         rotation_axis:str, 
         rotation_polar_constant:float, 
-        rotation_azimuth_constant:float) -> None: 
+        rotation_azimuth_constant:float,
+        hold_the_field_after_measurement:bool,
+        return_the_rotationstation:bool) -> None: 
         
         
         self.set_automaticstation = set_automaticstation
@@ -119,6 +121,8 @@ class FMRMode():
         self.rotation_axis = rotation_axis
         self.rotation_polar_constant = rotation_polar_constant
         self.rotation_azimuth_constant = rotation_azimuth_constant
+        self.hold_the_field_after_measurement=hold_the_field_after_measurement
+        self.return_the_rotationstation = return_the_rotationstation
         ## parameter initialization 
         
         
@@ -217,11 +221,14 @@ class FMRMode():
         if self.rotationstation: 
             try:
                 self.rotationstation_obj = RotationStage(self.rotationstation_port)
-                match self.rotation_axis:
-                    case "Polar": 
-                        self.rotationstation_obj.goToAzimuth(self.rotation_azimuth_constant)
-                    case "Azimuthal": 
-                        self.rotationstation_obj.goToPolar(self.rotation_polar_constant)
+               
+                self.rotationstation_obj.goToAzimuth(self.rotation_azimuth_constant)
+                while self.rotationstation_obj.checkBusyAzimuth() == 'BUSY;':
+                    sleep(0.01)
+                    
+                self.rotationstation_obj.goToPolar(self.rotation_polar_constant)
+                while self.rotationstation_obj.checkBusyPolar() == 'BUSY;':
+                    sleep(0.01)
             except:
                 log.error("Rotation station is not initialized")
                 self.rotationstation_obj = RotationStageDummy(self.rotationstation_port)
@@ -369,9 +376,10 @@ class FMRMode():
         FMRMode.idle(self)
 
     def idle(self):
-        sweep_field_to_zero(self.tmp_field, self.field_constant, self.field_step, self.field_obj)
+        if self.hold_the_field_after_measurement==False:
+            sweep_field_to_zero(self.tmp_field, self.field_constant, self.field_step, self.field_obj)
         self.generator_obj.setOutput(False, True)
-        if self.rotationstation: 
+        if self.return_the_rotationstation and self.rotationstation == True: 
             self.rotationstation_obj.goToZero()
 
 
