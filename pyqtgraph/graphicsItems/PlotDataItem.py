fix normalization
In cases where min(y) is equal to max(y) (e.g., when len(y) is equal to 1), the normalize function returned a List instead of the expected np.array

pyqtgraph/graphicsItems/PlotDataItem.py
@@ -1198,7 +1198,7 @@ class PlotDataItem(GraphicsObject):
         if len(y) == 0:
             return x, y
         if np.max(y) == np.min(y):
-            return x, [0.5] * len(y)
+            return x, np.array([0.5] * len(y))
         return x, (y - np.min(y)) / (np.max(y) - np.min(y))
 
     def _fourierTransform(self, x, y):