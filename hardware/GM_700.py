9121092f472df336fc92117887ee053f97cb1a5d
dmaslanka <dmaslanka@student.agh.edu.pl>
2024-09-11 13:32:24 +0200

    add driver

---
+++ hardware/GM_700.py
@@ -0,0 +1,30 @@
+from pymeasure.instruments import Instrument
+import pyvisa
+import time
+
+
+class GM700(Instrument):
+    def __init__(self, resource_name, **kwargs):
+        kwargs.setdefault("baud_rate", 9600)
+        kwargs.setdefault("data_bits", 8)
+        kwargs.setdefault("parity", pyvisa.constants.Parity.none)
+        kwargs.setdefault("stop_bits", pyvisa.constants.StopBits.one)
+        kwargs.setdefault("write_termination", "\r")
+        kwargs.setdefault("read_termination", "\n")
+        super().__init__(resource_name, "GM700", **kwargs)
+
+    def query(self, command):
+        """Send command and ignore the echoed command in the response"""
+        self.write(command)
+        time.sleep(0.1)
+        echo = self.read()
+        response = self.read()
+        return response
+
+
+if __name__ == "__main__":
+    device = GM700("ASRL7::INSTR")
+    response = device.query("*IDN?")
+    print(f"Response: {response}")
+
+
