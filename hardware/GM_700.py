412b6d0bfcc75db683dce57dce653caea5eef173
dmaslanka <dmaslanka@student.agh.edu.pl>
2024-09-13 11:55:00 +0200

    change to pyserial

--- hardware/GM_700.py
+++ hardware/GM_700.py
@@ -1,32 +1,29 @@
-from pymeasure.instruments import Instrument
-import pyvisa
+import serial
 import time
 
 
-class GM700(Instrument):
-    def __init__(self, resource_name, **kwargs):
-        kwargs.setdefault("baud_rate", 9600)
-        kwargs.setdefault("data_bits", 8)
-        kwargs.setdefault("parity", pyvisa.constants.Parity.none)
-        kwargs.setdefault("stop_bits", pyvisa.constants.StopBits.one)
-        kwargs.setdefault("write_termination", "\r")
-        kwargs.setdefault("read_termination", "\n")
-        super().__init__(resource_name, "GM700", **kwargs)
-
-    def query(self, command):
-        """Send command and ignore the echoed command in the response"""
-        self.write(command)
-        time.sleep(0.1)
-        echo = self.read()
-        response = self.read()
+class GM700():
+    def __init__(self, port, read_term='\n', write_term='\r'):
+        self.port = port
+        self.ser = serial.Serial(port, baudrate=9600, timeout=1)
+        self.read_term = read_term
+        self.write_term = write_term
+        
+    def query(self, command, query_delay=0.1):
+        self.ser.write(command.encode())
+        time.sleep(query_delay)
+        echo = self.ser.readline().decode().strip()
+        response = self.ser.readline().decode().strip()
         return response
+    
+    def shutdown(self):
+        self.ser.close()
+        print("Connection closed")
 
 
 if __name__ == "__main__":
-    device = GM700("ASRL8::INSTR")
-    response = device.query("*IDN?")
-    print(f"Response: {response}")
-    
-    device.shutdown()
+    gm = GM700("COM8")
+    print(gm.query("*IDN?\n"))
+    gm.shutdown()
 
 
