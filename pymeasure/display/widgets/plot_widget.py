66fbb16bb921244b3f6906ed047677ab6036851e
Did-Mas <102259306+Did-Mas@users.noreply.github.com>
2024-09-16 10:53:55 +0200

    make grid enabled by default

--- pymeasure/display/widgets/plot_widget.py
+++ pymeasure/display/widgets/plot_widget.py
@@ -86,6 +86,8 @@ class PlotWidget(TabWidget, QtWidgets.QWidget):
         )
         self.updated = self.plot_frame.updated
         self.plot = self.plot_frame.plot
+        self.plot.ctrl.xGridCheck.setChecked(True)
+        self.plot.ctrl.yGridCheck.setChecked(True)
         self.columns_x.setCurrentIndex(0)
         self.columns_y.setCurrentIndex(1)
 
