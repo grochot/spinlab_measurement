868b2546250c0083bee12eb3373cb11831433585
dmaslanka <dmaslanka@student.agh.edu.pl>
2024-09-16 14:31:06 +0200

    change to pyvisa

--- hardware/GM_700.py
+++ hardware/GM_700.py
@@ -1,29 +1,29 @@
-import serial
+import pyvisa
 import time
 
 
-class GM700():
-    def __init__(self, port, read_term='\n', write_term='\r'):
+class GM700:
+    def __init__(self, port, read_terminator="\n", write_terminator="\r"):
         self.port = port
-        self.ser = serial.Serial(port, baudrate=9600, timeout=1)
-        self.read_term = read_term
-        self.write_term = write_term
-        
-    def query(self, command, query_delay=0.1):
-        self.ser.write(command.encode())
-        time.sleep(query_delay)
-        echo = self.ser.readline().decode().strip()
-        response = self.ser.readline().decode().strip()
-        return response
-    
-    def shutdown(self):
-        self.ser.close()
-        print("Connection closed")
+        self.rm = pyvisa.ResourceManager()
+        self.inst = self.rm.open_resource(
+            port,
+            baud_rate=9600,
+            timeout=3000,
+            parity=pyvisa.constants.Parity.none,
+            data_bits=8,
+            stop_bits=pyvisa.constants.StopBits.one,
+            read_termination=read_terminator,
+            write_termination=write_terminator,
+        )
 
 
 if __name__ == "__main__":
-    gm = GM700("COM8")
-    print(gm.query("*IDN?\n"))
-    gm.shutdown()
-
-
+    gm = GM700("ASRL8::INSTR")
+    gm.inst.write("")
+    time.sleep(1)
+    gm.inst.write("*IDN?")
+    print(gm.inst.read())
+    time.sleep(0.1)
+    print(gm.inst.read())
+    # gm.inst.close()
