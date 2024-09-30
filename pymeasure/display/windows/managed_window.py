95b4916d825bc8f8044aa1df7cb0ee8937e3677f
dmaslanka <dmaslanka@student.agh.edu.pl>
2024-09-30 13:10:30 +0200

    refactor, cleanup

--- pymeasure/display/windows/managed_window.py
+++ pymeasure/display/windows/managed_window.py
@@ -372,12 +372,11 @@ class ManagedWindowBase(QtWidgets.QMainWindow):
         menu.exec(self.queue_button.mapToGlobal(position))
         
     def repeat_experiment(self):
-        # open times dialog
         dialog = QtWidgets.QInputDialog(self)
         dialog.setInputMode(QtWidgets.QInputDialog.InputMode.IntInput)
         dialog.setIntRange(1, 100)
         dialog.setIntValue(1)
-        dialog.setWindowTitle("Repeat Experiment")
+        dialog.setWindowTitle("Repeat")
         dialog.setLabelText("Number of times to repeat the experiment:")
         dialog.setOkButtonText("Queue")
         dialog.setCancelButtonText("Cancel")
