2794bbeddb22bed6c4298571e2a277a999449e7e
dmaslanka <dmaslanka@student.agh.edu.pl>
2024-10-16 23:33:24 +0200

    add abort callback to field_sweep_functions

--- logic/sweep_field_to_value.py
+++ logic/sweep_field_to_value.py
@@ -3,7 +3,7 @@ from time import sleep
 from hardware.daq import DAQ
 
 
-def sweep_field_to_value(start_field: float, end_field: float, field_step: float, daq: DAQ) -> float:
+def sweep_field_to_value(start_field: float, end_field: float, field_step: float, daq: DAQ, abort_callback=None) -> float:
     """
     Gradually sweeps the magnetic field from a starting value to a target value, adjusting the voltage in increments
     to ensure a smooth transition.
@@ -13,6 +13,7 @@ def sweep_field_to_value(start_field: float, end_field: float, field_step: float
         end_field (float): The target magnetic field value in Oersteds (Oe).
         field_step (float): The increment size for changing the magnetic field in Oersteds (Oe).
         daq (object): The DAQ device used to set the field during the sweep.
+        abort_callback (function, optional): A function that returns True if the sweep should be aborted. Defaults to None.
 
     Returns:
         float: The final magnetic field value that was set.
@@ -27,6 +28,11 @@ def sweep_field_to_value(start_field: float, end_field: float, field_step: float
     print(f"Sweeping field from: {start_field} to: {end_field} with {len(field_values)} steps: {field_values}")
 
     for field in field_values:
+        
+        if abort_callback and abort_callback():
+            print("Aborting sweep")
+            break
+        
         daq.set_field(field)
         last_set_field = field
         sleep(0.3)
--- logic/sweep_field_to_zero.py
+++ logic/sweep_field_to_zero.py
@@ -4,7 +4,7 @@ from time import sleep
 from hardware.daq import DAQ
 
 
-def sweep_field_to_zero(start_field: float, field_constant: float, field_step: float, daq: DAQ) -> float:
+def sweep_field_to_zero(start_field: float, field_constant: float, field_step: float, daq: DAQ, abort_callback=None) -> float:
     """
     Gradually sweeps the magnetic field from a starting value to zero, adjusting the voltage in increments to avoid abrupt changes.
 
@@ -14,6 +14,7 @@ def sweep_field_to_zero(start_field: float, field_constant: float, field_step: f
                                 If greater than 2, the field is directly set to zero.
         field_step (float): The step size for decreasing the field value in Oersteds (Oe).
         daq (object): The DAQ device used to set the field during the sweep.
+        abort_callback (function, optional): A function that returns True if the sweep should be aborted. Defaults to None.
 
     Returns:
         float: The final magnetic field value that was set.
@@ -22,16 +23,21 @@ def sweep_field_to_zero(start_field: float, field_constant: float, field_step: f
     if field_constant > 2:  # tego if'a bym wywalil przy mergowaniu
         daq.set_field(0)
         return 0.0
-    else:
-        step_direction = field_step if start_field < 0 else -field_step
-        field_values = np.arange(start_field, 0, step_direction)
-        field_values = np.append(field_values, 0)
 
-        print(f"Sweeping field from: {start_field} to: 0 with {len(field_values)} steps: {field_values}")
+    step_direction = field_step if start_field < 0 else -field_step
+    field_values = np.arange(start_field, 0, step_direction)
+    field_values = np.append(field_values, 0)
 
-        for field in field_values:
-            daq.set_field(field)
-            last_set_field = field
-            sleep(0.3)
+    print(f"Sweeping field from: {start_field} to: 0 with {len(field_values)} steps: {field_values}")
+
+    for field in field_values:
+        
+        if abort_callback and abort_callback():
+            print("Aborting sweep")
+            break
+        
+        daq.set_field(field)
+        last_set_field = field
+        sleep(0.3)
 
     return last_set_field
